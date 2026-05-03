fb7348fec84e3705cfc1dc7a069f46a6a4e7c4d3
Mateus Macedo <mateusmacedoeng@gmail.com>
2026-05-03 07:04:35 -0300

    fix bug, rename authenticate controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module.js'
-import { AuthenticateController } from './controllers/authenticate-controller.js'
+import { AuthenticateController } from './controllers/authenticate.controller.js'
 import { CreateAccountController } from './controllers/create-account.controller.js'
 import { envSchema } from './env.js'
 import { PrismaService } from './prisma/prisma.service.js'
