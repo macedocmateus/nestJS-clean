dd2bea1c3290aaa20494890d98be50677a4bffdb
Mateus Macedo <mateusmacedoeng@gmail.com>
2026-05-03 06:33:07 -0300

    add authentication controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module.js'
+import { AuthenticateController } from './controllers/authenticate-controller.js'
 import { CreateAccountController } from './controllers/create-account.controller.js'
 import { envSchema } from './env.js'
 import { PrismaService } from './prisma/prisma.service.js'
@@ -10,8 +12,9 @@ import { PrismaService } from './prisma/prisma.service.js'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
-  controllers: [CreateAccountController],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
