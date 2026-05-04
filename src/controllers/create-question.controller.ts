c3087455213cc5516536c040be59b5cca990a3a8
Mateus Macedo <mateusmacedoeng@gmail.com>
2026-05-04 16:56:05 -0300

    add create a question controller

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
     Controller,
     Post,
     UseGuards
@@ -6,14 +7,52 @@ import {
 import { JwtAuthGuard } from '../auth/jwt-auth.guard.js'
 import { CurrentUser } from '../auth/current-user-decorator.js'
 import type { UserPayload } from '../auth/jwt.strategy.js'
+import z from 'zod'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe.js'
+import { PrismaService } from '../prisma/prisma.service.js'
+
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string()
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor() {}
+  constructor(
+    private prisma: PrismaService
+  ) {}
 
   @Post()
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log(user.sub)
+  async handle(
+    @Body(bodyValidationPipe ) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload) {
+    const {title, content} = body
+    const userId = user.sub
+
+    const slug = this.convertToSlug(title)
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      }
+    })
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^\w\s-]/g, "")
+    .replace(/\s+/g, "-")
   }
 }
